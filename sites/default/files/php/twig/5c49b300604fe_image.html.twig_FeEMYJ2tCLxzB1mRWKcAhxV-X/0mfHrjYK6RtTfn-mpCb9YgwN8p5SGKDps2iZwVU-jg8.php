7e408c0732164c775ac3152d3dbd0f0e20cc16f5
Tenzin Sean <tenzin.jeunesse@gmail.com>
2019-01-25 16:00:49 +0100

    update all

---
+++ sites/default/files/php/twig/5c49b300604fe_image.html.twig_FeEMYJ2tCLxzB1mRWKcAhxV-X/0mfHrjYK6RtTfn-mpCb9YgwN8p5SGKDps2iZwVU-jg8.php
@@ -0,0 +1,76 @@
+<?php
+
+/* themes/custom/endymion/templates/field/image.html.twig */
+class __TwigTemplate_03a47b4c19125a6bea335884eab6956edb5e9544f0ed00bc3a4bcb314cc9326d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<img";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["attributes"] ?? null), "html", null, true));
+        echo " />
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/endymion/templates/field/image.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 13,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/custom/endymion/templates/field/image.html.twig", "/Applications/MAMP/htdocs/theme_drupal/themes/custom/endymion/templates/field/image.html.twig");
+    }
+}
